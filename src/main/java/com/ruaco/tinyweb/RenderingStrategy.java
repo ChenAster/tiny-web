49652e8577e512304364aecc6bdd26b9effe4ef6
Darkkksoul <mxl73@outlook.com>
2018-05-07 03:36:55 +0800

    rending interface, using in View constructor

---
+++ src/main/java/com/ruaco/tinyweb/RenderingStrategy.java
@@ -0,0 +1,9 @@
+package com.ruaco.tinyweb;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RenderingStrategy {
+
+    public String renderView(Map<String, List<String>> model);
+}
