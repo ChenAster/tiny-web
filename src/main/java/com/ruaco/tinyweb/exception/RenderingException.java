7bf10c30b760296aafcc31811d3ac8b5d9327e29
Darkkksoul <mxl73@outlook.com>
2018-05-07 03:32:16 +0800

    + runtime exception for rendering response in controller

---
+++ src/main/java/com/ruaco/tinyweb/exception/RenderingException.java
@@ -0,0 +1,8 @@
+package com.ruaco.tinyweb.exception;
+
+public class RenderingException extends RuntimeException {
+
+    public RenderingException(Throwable cause) {
+        super(cause);
+    }
+}
